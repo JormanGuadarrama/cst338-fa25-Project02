60a5ba0e6da74807bf72834e5cd397472d2e7f80
Nate Towsley <natetowsley@gmail.com>
2025-11-15 12:06:48 -0800

    Added current viable constructor, getters/setters, equals, and hashcode functions to User.java

--- app/src/main/java/com/example/project02/Database/Entities/User.java
+++ app/src/main/java/com/example/project02/Database/Entities/User.java
@@ -5,6 +5,8 @@ import androidx.room.PrimaryKey;
 
 import com.example.project02.Database.PantryManagerDatabase;
 
+import java.util.Objects;
+
 @Entity(tableName = PantryManagerDatabase.USER_TABLE)
 public class User {
     @PrimaryKey(autoGenerate = true)
@@ -13,4 +15,53 @@ public class User {
     private String password;
     private boolean isAdmin;
 
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && isAdmin == user.isAdmin && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, isAdmin);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
