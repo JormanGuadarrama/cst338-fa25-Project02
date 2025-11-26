6747ad1f15f3c584af8b257197f43ec87149c607
JormanGuadarrama <jguadarrama@csumb.edu>
2025-11-25 16:35:19 -0800

    added logout button on account page

--- app/src/main/java/com/example/project02/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/project02/ui/account/AccountFragment.java
@@ -1,12 +1,15 @@
 package com.example.project02.ui.account;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import com.example.project02.LoginActivity;
 import com.example.project02.R;
 
 public class AccountFragment extends Fragment {
@@ -15,6 +18,17 @@ public class AccountFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_account, container, false);
         final TextView textView = root.findViewById(R.id.text_account);
         textView.setText("This is the Account fragment");
+
+        Button logoutButton = root.findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
         return root;
     }
 }
